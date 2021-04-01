7e3d8a3f4be1e68be24a410bbedea835e7bb24d3
Allakot-Harsha <harshamohan023@gmail.com>
2021-04-01 08:33:19 +0530

    commit

--- app.js
+++ app.js
@@ -20,16 +20,3 @@ arrows.forEach((arrow, i) => {
   console.log(Math.floor(window.innerWidth / 270));
 });
 
-//TOGGLE
-
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
-);
-
-ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-  ball.classList.toggle("active");
-});
